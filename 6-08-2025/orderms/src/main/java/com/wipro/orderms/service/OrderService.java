f8d73a263f5fc7ddd594bf6fde2654fb600bf319
soujanya-08 <soujanyatodkar@gmail.com>
2025-08-25 19:59:54 +0530

    added

---
+++ 6-08-2025/orderms/src/main/java/com/wipro/orderms/service/OrderService.java
@@ -0,0 +1,14 @@
+package com.wipro.orderms.service;
+import java.util.List;
+
+import com.wipro.orderms.dto.OrderResponse;
+import com.wipro.orderms.entity.Order;
+
+
+
+public interface OrderService {
+
+	void save(Order order);
+	//List<Order> findAll();
+	List<OrderResponse> findAll();
+}
---
+++ 6-08-2025/orderms/src/main/java/com/wipro/orderms/service/impl/OrderServiceImpl.java
@@ -0,0 +1,99 @@
+package com.wipro.orderms.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.wipro.orderms.dto.OrderResponse;
+import com.wipro.orderms.dto.Payment;
+import com.wipro.orderms.entity.Order;
+import com.wipro.orderms.repo.OrderRepo;
+import com.wipro.orderms.service.OrderService;
+import com.wipro.orderms.service.PaymentConnectService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+	RestTemplate restTemplate;
+    
+    @Autowired
+	PaymentConnectService paymentConnectService;
+
+    @Override
+    @CircuitBreaker(name="order-cb", fallbackMethod="handleFallBack")
+    public void save(Order order) {
+        order.setOrderStatus("I");
+        orderRepo.save(order);
+        
+       // RestTemplate  rest= new RestTemplate();
+		//String url="http://localhost:9012/payment";
+        String url="http://paymentms/payment";
+        Payment payment = new Payment();
+        payment.setOrderId(order.getId());
+        payment.setPaymentAmount(order.getOrderValue());
+        payment.setPaymentStatus(true);
+
+       // try {
+        //	ResponseEntity<Payment>   response= restTemplate.postForEntity(url, payment, Payment.class);
+        	
+        ResponseEntity<Payment> response = paymentConnectService.savePaymentData(payment);
+
+        if (response.getStatusCode()== HttpStatusCode.valueOf(200)) {
+            order.setOrderStatus("P");
+        } 
+       // }catch(Exception ex){
+       // 	order.setOrderStatus("C");
+      //  }
+        orderRepo.save(order);
+    }
+
+//    @Override
+//    public List<Order> findAll() {
+//        return orderRepo.findAll();
+//    }
+    
+    @Override
+    
+    public List<OrderResponse> findAll() {
+        List<Order> orders = orderRepo.findAll();
+        List<OrderResponse> responses = new ArrayList<>();
+        System.out.println("findall");
+        for (Order order : orders) {
+            String url = "http://paymentms/payment/order/" + order.getId();
+            
+            Payment payment = null;
+            try {
+            //	ResponseEntity<Payment> response = paymentConnectService.findPaymentByOrderId(order.getId());
+                payment = restTemplate.getForObject(url, Payment.class);
+            } catch (Exception e) {
+                System.out.println("Payment service not available for orderId: " + order.getId());
+            }
+            
+            OrderResponse response = new OrderResponse();
+            response.setOrder(order);
+            response.setPayment(payment);
+            responses.add(response);
+        }
+
+        return responses;
+    }
+
+  
+    void handleFallBack(Order order, Throwable t) {
+        System.out.println("--System is down-- ");
+       // order.setOrderStatus("C");
+        //orderRepo.save(order);
+    }
+
+}
